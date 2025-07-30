102465e35e1ebfd7e0c9d3ee752e95a4cfeff4b6
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2025-07-30 19:19:13 +0600

    convestion api added

--- routes/liveChatRoute.js
+++ routes/liveChatRoute.js
@@ -8,7 +8,7 @@ router.get("/online-users", async (req, res) => {
     const onlineUserQuery = `SELECT * FROM live_support_online_users WHERE is_online = ${isOnline}`;
     const users = await QueryDocument(onlineUserQuery);
 
-    res.send(users);
+    res.send({ status: "success", data: users });
   } catch (error) {
     throw error;
   }
@@ -19,7 +19,7 @@ router.get("/conversations", async (_req, res) => {
     const { page = 1, limit = 30, status, user_id } = _req.query;
 
     let conversationsQuery =
-      "SELECT users.name AS user_name, users.profile_image AS user_image, admin.name AS admin_name, admin.profile_image AS admin_image, lsm.* FROM live_support_message_request AS lsm INNER JOIN users ON users.id = lsm.user_id LEFT JOIN users AS admin ON admin.id = lsm.admin_id";
+      "SELECT users.name AS user_name, users.profile_image AS user_image, admin.name AS admin_name, admin.profile_image AS admin_image, online_users.is_online AS user_online, lsm.* FROM live_support_message_request AS lsm INNER JOIN users ON users.id = lsm.user_id LEFT JOIN users AS admin ON admin.id = lsm.admin_id LEFT JOIN live_support_online_users AS online_users ON online_users.user_id = lsm.user_id";
 
     if (status && user_id) {
       conversationsQuery += ` WHERE lsm.status = '${status}' AND lsm.user_id = ${user_id}`;
@@ -32,7 +32,14 @@ router.get("/conversations", async (_req, res) => {
     conversationsQuery += ` LIMIT ${limit} OFFSET ${(page - 1) * limit}`;
 
     const conversations = await QueryDocument(conversationsQuery);
-    res.send(conversations);
+
+    //count total document
+    const totalQuery =
+      "SELECT COUNT(*) AS total FROM live_support_message_request";
+    const total = await QueryDocument(totalQuery);
+    const totalPage = Math.ceil(total[0].total / limit);
+
+    res.send({ status: "success", totalPage, data: conversations });
   } catch (error) {
     throw error;
   }
@@ -49,7 +56,7 @@ router.get("/conversations/:id", async (req, res) => {
       ...conversations[0],
       messages: messages,
     };
-    res.send(data);
+    res.send({ status: "success", data: data });
   } catch (error) {
     throw error;
   }
